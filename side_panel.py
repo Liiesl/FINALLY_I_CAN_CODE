8e70351c24822ef5100fa0a3fd07c41c681aa7d4
Liiesl <gobloklo777@gmail.com>
2025-01-23 18:07:25 +0700

    Update side_panel.py

--- side_panel.py
+++ side_panel.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PyQt5.QtCore import Qt
 
 class SidePanel(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, open_settings_callback=None):
         super().__init__(parent)
         self.setStyleSheet("background-color: #1e1e1e;")
         self.setFixedWidth(parent.width() // 2)
@@ -14,3 +14,11 @@ class SidePanel(QWidget):
 
         self.layout().addWidget(self.info_label)
         self.layout().addStretch()
+
+        # Add settings link
+        self.settings_label = QLabel("<a href='#'>Settings</a>")
+        self.settings_label.setStyleSheet("color: #4f86f7; font-size: 16px;")
+        self.settings_label.setAlignment(Qt.AlignCenter)
+        self.settings_label.setOpenExternalLinks(False)
+        self.settings_label.linkActivated.connect(open_settings_callback)
+        self.layout().addWidget(self.settings_label)
