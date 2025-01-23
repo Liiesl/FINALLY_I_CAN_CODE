5fd427679c3253118470a534f696d358f3d86f9a
Liiesl <gobloklo777@gmail.com>
2025-01-23 18:18:55 +0700

    Update side_panel.py

--- side_panel.py
+++ side_panel.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QListWidget, QListWidgetItem
 from PyQt5.QtCore import Qt
 
 class SidePanel(QWidget):
@@ -9,16 +9,21 @@ class SidePanel(QWidget):
         self.setLayout(QVBoxLayout())
 
         self.info_label = QLabel("Side Panel Content")
-        self.info_label.setStyleSheet("color: white; font-size: 18px; font-weight: bold;")
+        self.info_label.setStyleSheet("color: white; font-size: 18px; font-weight: bold; background-color: transparent;")
         self.info_label.setAlignment(Qt.AlignCenter)
 
         self.layout().addWidget(self.info_label)
         self.layout().addStretch()
 
-        # Add settings link
-        self.settings_label = QLabel("<a href='#'>Settings</a>")
-        self.settings_label.setStyleSheet("color: #4f86f7; font-size: 16px;")
-        self.settings_label.setAlignment(Qt.AlignCenter)
-        self.settings_label.setOpenExternalLinks(False)
-        self.settings_label.linkActivated.connect(open_settings_callback)
-        self.layout().addWidget(self.settings_label)
+        # Create a list widget for settings
+        self.settings_list = QListWidget()
+        self.settings_list.setStyleSheet("background-color: transparent; border: none; color: #4f86f7; font-size: 16px;")
+
+        settings_item = QListWidgetItem("Settings")
+        settings_item.setTextAlignment(Qt.AlignLeft)
+        settings_item.setFlags(settings_item.flags() | Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+        self.settings_list.addItem(settings_item)
+
+        self.settings_list.itemClicked.connect(lambda: open_settings_callback())
+
+        self.layout().addWidget(self.settings_list)
