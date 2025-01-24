fda765b98ae5baf6761b5c64a9aaa22b32d3aa06
Liiesl <gobloklo777@gmail.com>
2025-01-24 22:58:41 +0700

    Update side_panel.py

--- side_panel.py
+++ side_panel.py
@@ -1,12 +1,14 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QListWidget, QListWidgetItem
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QPalette
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtGui import QFont, QPalette, QDesktopServices
 from config import Config
+import os
 
 class SidePanel(QWidget):
     def __init__(self, parent=None, open_settings_callback=None):
         super().__init__(parent)
         self.config = Config()
+        self.open_settings_callback = open_settings_callback
         self.setup_ui(open_settings_callback)
         self.setFont(QFont("Inter Regular"))
 
@@ -48,11 +50,26 @@ class SidePanel(QWidget):
         settings_item.setFlags(settings_item.flags() | Qt.ItemIsSelectable | Qt.ItemIsEnabled)
         self.settings_list.addItem(settings_item)
 
+        # Add changelog item to the list
+        changelog_item = QListWidgetItem("Changelog")
+        changelog_item.setTextAlignment(Qt.AlignLeft)
+        changelog_item.setFlags(changelog_item.flags() | Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+        self.settings_list.addItem(changelog_item)
+
         # Connect the item click event to the callback
-        self.settings_list.itemClicked.connect(open_settings_callback)
+        self.settings_list.itemClicked.connect(self.handle_item_clicked)
 
         self.layout().addWidget(self.settings_list)
         self.layout().insertWidget(1, self.settings_list)  # Insert the list at the top, below the info label
+
+    def handle_item_clicked(self, item):
+        if item.text() == "Settings":
+            # Handle the settings click event
+            self.open_settings_callback()
+        elif item.text() == "Changelog":
+            # Handle the changelog click event
+            changelog_path = os.path.join(os.path.dirname(__file__), 'assets', 'changelog.txt')
+            QDesktopServices.openUrl(QUrl.fromLocalFile(changelog_path))
 
     def apply_theme(self):
         # Retrieve the current palette colors
