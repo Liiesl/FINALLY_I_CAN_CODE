f88b0547cae7c51c6f2c85e2e4cc50d5fa16b339
Liiesl <gobloklo777@gmail.com>
2025-01-24 23:17:19 +0700

    Update changelog_window.py

--- assets/changelog/changelog_window.py
+++ assets/changelog/changelog_window.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QVBoxLayout, QWidget, QApplication
-from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtGui import QPalette, QApplication, QFont
 from PyQt5.QtCore import Qt
 import os
 
@@ -14,8 +14,16 @@ class ChangelogWindow(QMainWindow):
         
         self.layout = QVBoxLayout(self.central_widget)
         
+        # Create a QLabel for the title
+        self.title_label = QLabel("What's New")
+        self.title_label.setAlignment(Qt.AlignCenter)
+        self.title_label.setFont(QFont("Inter ExtraBold", 50))
+        self.layout.addWidget(self.title_label)
+        
+        # Create a QTextEdit for the changelog content
         self.text_edit = QTextEdit()
         self.text_edit.setReadOnly(True)
+        self.text_edit.setAlignment(Qt.AlignCenter)  # Align the text to center
         self.layout.addWidget(self.text_edit)
         
         self.apply_palette()
@@ -27,6 +35,7 @@ class ChangelogWindow(QMainWindow):
         background_color = palette.color(QPalette.Window).name()
         
         self.setStyleSheet(f"background-color: {background_color}; color: {text_color};")
+        self.title_label.setStyleSheet(f"background-color: {background_color}; color: {text_color};")
         self.text_edit.setStyleSheet(f"background-color: {background_color}; color: {text_color};")
 
     def load_changelog(self):
