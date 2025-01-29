1e15c609f823477c3ffd93e3775629632ef3e501
Liiesl <gobloklo777@gmail.com>
2025-01-29 15:55:43 +0700

    Update settings.py

--- assets/modules/settings.py
+++ assets/modules/settings.py
@@ -145,8 +145,6 @@ class Settings(QWidget):
 
         self.config.save()
 
-        self.custom_window_bar.apply_theme()
-
         self.config.load()
         if self.main_window:
             self.main_window.refresh_settings()
