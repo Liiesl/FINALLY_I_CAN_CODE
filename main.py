04d8579f7923af01d0aeb47cd7c24c113cd7d79f
Liiesl <gobloklo777@gmail.com>
2025-01-30 20:58:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ class MainWindow(QMainWindow):
         self.menu_button = None
 
         self.create_new_tab_content()
+        
+        self.tab_names = {}
 
     def apply_theme(self):
         self.config = Config(source="MainWindow")
@@ -264,6 +266,9 @@ class MainWindow(QMainWindow):
 
         # Replicate the main menu layout in the new tab
         self.main_menu(new_main_content_layout)
+        
+        current_custom_index = self.custom_window_bar.tab_bar.currentIndex()
+        self.tab_names[current_custom_index] = "Subtl"
 
     def remove_tab_content(self, index):
         widget = self.tab_contents.widget(index)
@@ -272,7 +277,12 @@ class MainWindow(QMainWindow):
             widget.deleteLater()  # Clean up the widget
 
     def display_tab_content(self, index):
-        self.tab_contents.setCurrentIndex(index)
+        main_window_index = index - 1
+        if main_window_index >= 0 and main_window_index < self.tab_contents.count():
+            self.tab_contents.setCurrentIndex(main_window_index)
+
+        title = self.tab_names.get(index, "Subtl")
+        self.custom_window_bar.tab_bar.setTabText(index, title)
 
     def create_tool_button(self, tool_name, tool_description):
         button = QPushButton()
@@ -415,6 +425,10 @@ class MainWindow(QMainWindow):
             self.apply_theme()
             self.update_tool_button_visibility()
             self.resizeEvent = self.update_tool_button_visibility
+        
+            current_custom_index = self.custom_window_bar.tab_bar.currentIndex()
+            self.tab_names[current_custom_index] = "Subtl"
+            self.custom_window_bar.tab_bar.setTabText(current_custom_index, "Subtl")
 
     def tool_selected(self, tool_name):
         # Get the current splitter for the active tab
@@ -445,7 +459,11 @@ class MainWindow(QMainWindow):
                 self.load_tool(tool_widget, main_content_layout)
             else:
                 QMessageBox.information(self, "Coming Soon", "This feature is coming soon!")
-
+        
+            current_custom_index = self.custom_window_bar.tab_bar.currentIndex()
+            self.tab_names[current_custom_index] = tool_name
+            self.custom_window_bar.tab_bar.setTabText(current_custom_index, tool_name)
+    
     def load_tool(self, tool_widget, layout):
         self.main_menu_active = False
 
