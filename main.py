855d3e02d5ab836b1c0dd586b693641ee997647f
Liiesl <gobloklo777@gmail.com>
2025-01-30 22:25:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -416,10 +416,10 @@ class MainWindow(QMainWindow):
             self.update_tool_button_visibility()
             self.resizeEvent = self.update_tool_button_visibility
             
-            current_tab_contents_index = self.tab_contents.currentIndex()
-            tab_bar_index = current_tab_contents_index + 1  # Adjust for hidden tab
-            if tab_bar_index < self.custom_window_bar.tab_bar.count():
-                self.custom_window_bar.tab_bar.setTabText(tab_bar_index, "Subtl")
+        current_tab_contents_index = self.tab_contents.currentIndex()
+        tab_bar_index = current_tab_contents_index + 1  # Adjust for hidden tab
+        if tab_bar_index < self.custom_window_bar.tab_bar.count():
+            self.custom_window_bar.tab_bar.setTabText(tab_bar_index, "Subtl")
 
     def tool_selected(self, tool_name):
         # Get the current splitter for the active tab
@@ -451,10 +451,10 @@ class MainWindow(QMainWindow):
             else:
                 QMessageBox.information(self, "Coming Soon", "This feature is coming soon!")
 
-            current_tab_contents_index = self.tab_contents.currentIndex()
-            tab_bar_index = current_tab_contents_index + 1  # Adjust for hidden tab
-            if tab_bar_index < self.custom_window_bar.tab_bar.count():
-                self.custom_window_bar.tab_bar.setTabText(tab_bar_index, tool_name)
+        current_tab_contents_index = self.tab_contents.currentIndex()
+        tab_bar_index = current_tab_contents_index + 1  # Adjust for hidden tab
+        if tab_bar_index < self.custom_window_bar.tab_bar.count():
+             self.custom_window_bar.tab_bar.setTabText(tab_bar_index, tool_name)
         
     def load_tool(self, tool_widget, layout):
         self.main_menu_active = False
