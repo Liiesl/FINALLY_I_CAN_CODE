4eb68ff12b7e3f4d5e1ff58e2059fbed70f0fd08
Liiesl <gobloklo777@gmail.com>
2025-02-01 03:58:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -379,7 +379,7 @@ class MainWindow(QMainWindow):
                         background-color: {button_color};
                         color: {button_text_color};
                         border: 2px solid {palette.color(QPalette.Highlight).name()};
-                        border-radius: 30px;
+                        border-radius: 25px;
                         padding: 5px 5px 5px 35px;
                     }}
                     QLineEdit::placeholder {{
