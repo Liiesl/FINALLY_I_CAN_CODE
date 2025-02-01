4226e5ed459c3dbfb3549b6d0293e41a5376cc0c
Liiesl <gobloklo777@gmail.com>
2025-02-01 21:18:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -704,6 +704,10 @@ class MainWindow(QMainWindow):
         for category, btn in self.category_buttons.items():
             if btn.isChecked():
                 self.active_categories.add(category)
+        if self.active_categories:
+            self.on_tag_selected()
+        else:
+            self.on_tag_deselected()
         self.filter_tools(self.search_field.text())
 
     def filter_tools(self, search_text):
