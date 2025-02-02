eb51eda743522f521a809cc5fa1820b3ca81b289
Liiesl <gobloklo777@gmail.com>
2025-02-02 21:49:35 +0700

    Update help_window.py

--- assets/modules/help_window.py
+++ assets/modules/help_window.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 import markdown
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QSplitter, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QListWidgetItem, QSplitter, QPushButton
 from PyQt5.QtCore import Qt
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtGui import QFont
@@ -76,7 +76,7 @@ class HelpWindow(QWidget):
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(main_splitter)
 
-        # Left panel: List of sections (headers) using QTextEdit for word wrapping
+        # Left panel: List of sections (headers)
         left_panel = QWidget()
         left_layout = QVBoxLayout()
         left_panel.setLayout(left_layout)
@@ -87,20 +87,13 @@ class HelpWindow(QWidget):
         self.toggle_button.clicked.connect(self.toggle_navigation)
         left_layout.addWidget(self.toggle_button, alignment=Qt.AlignRight)
 
-        self.section_list = QTextEdit()
+        self.section_list = QListWidget()
         self.section_list.setFont(QFont("Arial", 14))  # Increase text size
-        self.section_list.setReadOnly(True)  # Make it non-editable
-        self.section_list.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)  # Enable vertical scrolling
-        self.section_list.setWordWrapMode(True)  # Enable word wrapping
-
-        # Populate the QTextEdit with headers
         for level, title in self.headers:
-            indent = (level - 1) * 10  # Reduced indentation multiplier
-            if level == 1:  # h1 headers should be aligned to the left
-                indent = 0
-            indented_title = f"{' ' * indent}{title}\n"
-            self.section_list.append(indented_title)
-
+            item = QListWidgetItem(title)
+            item.setData(Qt.UserRole, title)  # Store the header title for later use
+            self.section_list.addItem(item)
+        self.section_list.itemClicked.connect(self.scroll_to_section)
         left_layout.addWidget(self.section_list)
 
         # Right panel: Markdown viewer using QWebEngineView
@@ -114,15 +107,6 @@ class HelpWindow(QWidget):
         # Set initial sizes for the splitter
         main_splitter.setSizes([200, 600])
 
-    def toggle_navigation(self):
-        """Toggle the visibility of the navigation panel."""
-        if self.section_list.isVisible():
-            self.section_list.hide()
-            self.toggle_button.setIcon(qta.icon('fa5s.angle-right'))
-        else:
-            self.section_list.show()
-            self.toggle_button.setIcon(qta.icon('fa5s.angle-left'))
-
     def scroll_to_section(self, item):
         """Scroll to the selected section in the markdown viewer."""
         selected_title = item.data(Qt.UserRole)
@@ -134,6 +118,7 @@ class HelpWindow(QWidget):
         # Convert the title to lowercase and replace spaces with hyphens
         anchor = title.lower().replace(" ", "-")
         return anchor
+
     def toggle_navigation(self):
         """Toggle the visibility of the navigation panel."""
         if self.section_list.isVisible():
